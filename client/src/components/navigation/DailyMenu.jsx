565ad58cc9cc5855a1181135d36dd367e8b5d349
GenayaJames <genayakjames@gmail.com>
2018-05-26 18:01:20 -0400

    menu for daily view

--- client/src/components/navigation/DailyMenu.jsx
+++ client/src/components/navigation/DailyMenu.jsx
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom';
 
 function DailyMenu(props) {
   return (
-    <nav>
-      <Link to="/dashboard/daily">Tasks</Link>
-      <Link to="/dashboard/daily/events">Events</Link>
-      <Link to="/dashboard/daily/notes">Notes</Link>
-      <Link to="/dashboard/daily/schedule">Schedule</Link>
+    <nav className="daily-nav">
+      <Link to="/dashboard/daily" className="daily-tasks">Tasks</Link>
+      <Link to="/dashboard/daily/events" className="daily-events">Events</Link>
+      <Link to="/dashboard/daily/notes" className="daily-notes">Notes</Link>
+      <Link to="/dashboard/daily/schedule" className="daily-schedule">Schedule</Link>
     </nav>
   )
 }
