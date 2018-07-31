d4e059aaa6d67f78d0e94f9801b695d0b896db92
Genaya James <genayakjames@gmail.com>
2018-07-30 21:02:17 -0400

    remove unnecessary passed down props

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -7,38 +7,7 @@ import Service from '../services/authService';
 import Schedule from '../services/scheduleService';
 
 class App extends Component {
-
-  updateSchedule(schedule) {
-    Schedule.Update(schedule)
-    .then(data => {
-      this.setState((prevState) => {
-        this.fetchScheduleDates();
-        const index = prevState.schedules.findIndex(schedule => schedule.id === data.id);
-        return {
-          schedules: [
-            ...prevState.schedules.slice(0, index),
-            data,
-            ...prevState.schedules.slice(index + 1)
-          ]
-        }
-      })
-    });
-  }
-
-  deleteSchedule(id) {
-    Schedule.Delete(id)
-    .then(data => {
-      this.setState((prevState) => {
-        this.fetchScheduleDates();
-        return {
-          schedules: prevState.schedules.filter(schedule => schedule.id !== id)
-        }
-      })
-    })
-  }
-
-  // Auth
-
+  // Checks if user is still logged in
   isLoggedIn() {
     if (localStorage.getItem('authToken') !== null) {
       if(localStorage.getItem('authToken') !== "undefined") {
@@ -57,12 +26,7 @@ class App extends Component {
     return (
       <div>
         <main>
-          <Main
-            isLoggedIn={this.isLoggedIn}
-            createSchedule={this.createSchedule}
-            updateSchedule={this.updateSchedule}
-            deleteSchedule={this.deleteSchedule}
-          />
+          <Main isLoggedIn={this.isLoggedIn} />
         </main>
       </div>
     );
