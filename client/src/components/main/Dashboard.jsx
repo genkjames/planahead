c130523c301fa7e5e4ac3f5b0afa32509e2158ef
Genaya James <genayakjames@gmail.com>
2018-08-28 20:22:56 -0400

    redirect to dashboard when route is incomplete

--- client/src/components/main/Dashboard.jsx
+++ client/src/components/main/Dashboard.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import SideMenu from '../navigation/SideMenu';
 import TopNav from '../navigation/TopNav';
 import Monthly from './Monthly';
@@ -142,6 +142,7 @@ class Dashboard extends Component {
                 />
               )}
             />
+            <Redirect to="/dashboard" />
           </Switch>
         </div>
       </div>
